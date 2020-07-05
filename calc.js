49299156dee33b2cce931a294693002d72b35ab4
shivani jha <shivani261979@gmail.com>
2020-07-04 23:14:37 -0400

    simple logic

---
+++ calc.js
@@ -0,0 +1,35 @@
+function calcAdd(firstNumber,secondNumber)
+    {
+var a = parseInt(firstNumber);
+var b = parseInt(secondNumber);
+var c = a+b;
+alert ("Result =" +c);
+
+}
+
+function calcMinus(firstNumber,secondNumber)
+    {
+var a = parseInt(firstNumber);
+var b = parseInt(secondNumber);
+var c = a-b;
+alert ("Result=" + c);
+
+}
+
+function calcMultiplication(firstNumber,secondNumber)
+    {
+var a = parseInt(firstNumber);
+var b = parseInt(secondNumber);
+var c = a*b;
+alert ("Result="+ c);
+
+}
+
+function calcDivision(firstNumber,secondNumber)
+    {
+var a = parseInt(firstNumber);
+var b = parseInt(secondNumber);
+var c = a/b;
+alert ("Result="+c);
+
+}
